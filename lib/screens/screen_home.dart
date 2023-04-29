ca0b1622a748ef3aa3c5059015b18b95782523c0
mhmd-thaju <thajupm@gmail.com>
2023-04-29 21:29:18 +0530

    updated

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -8,16 +8,46 @@ class ScreenOne extends StatelessWidget {
     return Scaffold(
       //appBar: AppBar(title: Text("Screen")),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            decoration:
-                const BoxDecoration(color: Color.fromARGB(66, 255, 252, 252)),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [Text("Hello")],
-              ),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Color.fromARGB(66, 255, 252, 252),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                )
+              ]),
+          padding: const EdgeInsets.all(0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Welcome To OctoBus ",
+                  style: TextStyle(
+                    fontFamily: 'LobsterTwo',
+                    fontSize: 36.0,
+                    //fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Color.fromARGB(255, 18, 140, 239),
+                  ),
+                ),
+                SizedBox(
+                  height: 250,
+                ),
+                Text("Our Team :- "),
+                Text(''' 
+                Anjaly Krishna --
+                Muhammed Thajudheen --
+                Shyam Kiran --
+                Aasim Mohammed --AL MAIN
+                
+
+                Supervising by:-    Luttapi😈(SK)
+                '''),
+              ],
             ),
           ),
         ),
