140ed9e055ff77bcf8c1248603d438f3082bf833
mhmd-thaju <thajupm@gmail.com>
2023-05-01 23:08:53 +0530

    hvy

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -3,66 +3,187 @@ import 'package:flutter/material.dart';
 class ScreenOne extends StatelessWidget {
   const ScreenOne({super.key});
 
+  Widget _selectedArea(
+      {required Color color,
+      required String title,
+      required String subtitile,
+      required String image}) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      padding: EdgeInsets.only(left: 10),
+      height: 220,
+      width: 150,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage(image),
+            width: 125,
+            height: 150,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            subtitile,
+            style: TextStyle(fontSize: 15, color: Colors.black),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(title: Text("Screen")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          "Welcome to OctoBus",
+          style: TextStyle(
+            fontFamily: 'LobsterTwo',
+            fontSize: 32,
+            fontStyle: FontStyle.italic,
+            color: Color.fromARGB(255, 12, 138, 240),
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Container(
-          decoration: BoxDecoration(
-              color: Color.fromARGB(66, 255, 252, 252),
-              image: DecorationImage(
-                  image: AssetImage("asset/images/octobus.png"),
-                  fit: BoxFit.fitWidth,
-                  opacity: 199),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white.withOpacity(01.0),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3),
-                )
-              ]),
-          padding: const EdgeInsets.all(0),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                SizedBox(
-                  height: 33,
+          padding: EdgeInsetsDirectional.only(top: 15),
+          child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 60, 159, 240),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              //padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Padding(
+                padding: EdgeInsets.only(top: 25, left: 20, right: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      '''OctoBus is a Bus Monitoring App developed to asisst people those who uses bus form transportation in "Kerala"''',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(12, 12, 12, 0),
+                      height: 554.635,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30)),
+                          image: DecorationImage(
+                              image: AssetImage("asset/images/octobus.png"),
+                              fit: BoxFit.fitWidth,
+                              opacity: 199),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white.withOpacity(01.0),
+                              spreadRadius: 5,
+                              blurRadius: 1,
+                              offset: Offset(0, 3),
+                            )
+                          ]),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Our Team :-",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Row(
+                                children: [
+                                  _selectedArea(
+                                      color: Colors.white.withOpacity(0.7),
+                                      subtitile: "3488",
+                                      title: "Aasim Mohammed",
+                                      image: 'asset/images/Aasim.jpg'),
+                                  _selectedArea(
+                                      color: Colors.white.withOpacity(0.7),
+                                      subtitile: "3414",
+                                      title: "Shyam Kiran",
+                                      image: 'asset/images/shyam.jpg'),
+                                  _selectedArea(
+                                      color: Colors.white.withOpacity(0.7),
+                                      subtitile: "3401",
+                                      title: "Anjali Krishna",
+                                      image: 'asset/images/anjali.jpg'),
+                                  _selectedArea(
+                                      color: Colors.white.withOpacity(0.7),
+                                      subtitile: "3408",
+                                      title: '''Muhammed Thajudheen''',
+                                      image: 'asset/images/thaju.jpeg'),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 200,
+                          ),
+                          Text(
+                              '''App is currently in development Please wait for new features in the future updates''')
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  "Welcome To OctoBus ",
-                  style: TextStyle(
-                    fontFamily: 'LobsterTwo',
-                    fontSize: 36.0,
-                    //fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Color.fromARGB(255, 18, 140, 239),
-                  ),
-                ),
-                SizedBox(
-                  height: 250,
-                ),
-                Text("Our Team :- ",
-                    style: TextStyle(color: Color.fromARGB(255, 5, 36, 99))),
-                Text(
-                  ''' 
-                Anjaly Krishna --
-                Muhammed Thajudheen --
-                Shyam Kiran --
-                Aasim Mohammed --AL MAIN
-                
+              )
 
-                Supervising by:-    Luttapi😈(SK)
-                ''',
-                  style: TextStyle(color: Color.fromARGB(255, 5, 36, 99)),
-                ),
-              ],
-            ),
-          ),
+              //decoration: BoxDecoration(color: Colors.blue),
+              ),
         ),
       ),
     );
   }
 }
+
+
+
+// Column(
+//                 children: const [
+//                   Text(
+//                     "Welcome to OctoBus",
+//                     style: TextStyle(
+//                       fontFamily: 'LobsterTwo',
+//                       fontSize: 32,
+//                       fontStyle: FontStyle.italic,
+//                     ),
+//                   ),
+//                   SizedBox(
+//                     height: 50,
+//                   ),
+//                   Text("adasd")
+//                 ],
+//               ),
